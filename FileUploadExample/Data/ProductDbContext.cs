c5daac280b1cfa8449f8d63fc78c1af7cff69ab5
John Uthus <john.a.uthus.jr1@gmail.com>
2020-12-02 12:28:12 -0800

    Add project files.

---
+++ FileUploadExample/Data/ProductDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FileUploadExample.Models;
+
+namespace FileUploadExample.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext (DbContextOptions<ProductDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FileUploadExample.Models.Product> Product { get; set; }
+    }
+}
---
+++ FileUploadExample/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileUploadExample.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string ProductName { get; set; }
+
+        [DataType(DataType.Currency)]
+        public double Price { get; set; }
+
+        [NotMapped] // Does not get stroed in DB
+        public IFormFile ProductPhoto { get; set; }
+
+        /// <summary>
+        /// URL where the product is stored
+        /// </summary>
+        public string PhotoUrl { get; set; }
+
+    }
+}
